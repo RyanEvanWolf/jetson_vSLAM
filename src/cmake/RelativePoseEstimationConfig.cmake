20da3e537eac5b5d92f91346373eaf6ceeeeccf0
RyanEvanWolf <ryanevwolf@gmail.com>
2017-12-02 19:04:31 +0200

    Added link test for the motion estimation libraries, still having issues with having to add -DCUDA_USE_STATIC_CUDA_RUNTIME=OFF as an extra cmake variable

---
+++ src/cmake/RelativePoseEstimationConfig.cmake
@@ -0,0 +1,11 @@
+set(RelativePoseEstimation_LIBRARIES_DIR 
+/home/ubuntu/git_workspace/relative-pose-estimation/build/one-point
+/home/ubuntu/git_workspace/relative-pose-estimation/build/five-point-nister
+/home/ubuntu/git_workspace/relative-pose-estimation/build/four-point-groebner
+/home/ubuntu/git_workspace/relative-pose-estimation/build/four-point-numerical
+)
+set(RelativePoseEstimation_LIBS one-point five-point-nister four-point-groebner four-point-numerical gsl gslcblas m)
+set(RelativePoseEstimation_INCLUDE_DIRS
+/home/ubuntu/git_workspace/relative-pose-estimation
+)
+set(RelativePoseEstimation_FOUND TRUE)
