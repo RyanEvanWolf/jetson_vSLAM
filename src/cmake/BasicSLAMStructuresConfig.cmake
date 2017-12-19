f943d4099c2927f8a85ac7be50f021d58210e2f8
RyanEvanWolf <ryanevwolf@gmail.com>
2017-12-19 20:27:49 +0200

    added image_transport as a catkin build project + updates to the front_end package

--- src/cmake/BasicSLAMStructuresConfig.cmake
+++ src/cmake/BasicSLAMStructuresConfig.cmake
@@ -1,4 +1,4 @@
-set(Structures_LIBRARIES_DIR /home/ubuntu/git/BasicSLAMStructures/build/lib)
-set(Structures_LIBS BasicSLAMStructures boost_filesystem boost_system)
-set(Structures_INCLUDE_DIRS /home/ubuntu/git/BasicSLAMStructures/include/)
-set(Structures_FOUND TRUE)
+set(BasicSLAMStructures_LIBRARIES_DIR /home/ubuntu/git/BasicSLAMStructures/build/lib)
+set(BasicSLAMStructures_LIBS BasicSLAMStructures boost_filesystem boost_system)
+set(BasicSLAMStructures_INCLUDE_DIRS /home/ubuntu/git/BasicSLAMStructures/include/)
+set(BasicSLAMStructures_FOUND TRUE)
